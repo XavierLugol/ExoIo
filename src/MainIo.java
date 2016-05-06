b22c45da64b89970534ed559417161a764f0fe25
xavier <xavier.lugol@laposte.net>
2016-05-06 17:03:57 +0200

    En cours

--- src/MainIo.java
+++ src/MainIo.java
@@ -4,38 +4,66 @@ import java.nio.*;
 
 public class MainIo {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		String 	myReponse="";
-		
+		String myReponse = "";
+
 		System.out.println("Chemin d'origine :");
-		String 	myString = new String(sc.nextLine());
+		String myString = new String(sc.nextLine());
 		File mydir = new File(myString);
-		
+
 		if ((!(mydir.exists()) || (!mydir.isDirectory()))) {
 			mydir.mkdir();
 		}
-		
+
 		System.out.println("Voulez-vous une arborescence à 2 niveaux ? (O/N) :");
-		do
-		{
+		do {
 			myReponse = new String(sc.next());
+		} while (!myReponse.equals("N") && !myReponse.equals("O"));
+
+		if (myReponse.equals("O")) {
+			mydir = new File(myString + "/sub1");
+			if ((!(mydir.exists()) || (!mydir.isDirectory()))) {
+				mydir.mkdir();
+			}
+			mydir = new File(myString + "/sub1/sub2");
+			if ((!(mydir.exists()) || (!mydir.isDirectory()))) {
+				mydir.mkdir();
+			}
+			// Directory
+			writeDirFile(myString + "/sub1");
+			writeDirFile(myString + "/sub1/sub2");
 		}
-		while (!myReponse.equals("N") && !myReponse.equals("O"));
+		writeDirFile(myString);
 		
-		if (myReponse.equals("O"))
-		{
-			mydir = new File(myString+"/sub1");
-			if ((!(mydir.exists()) || (!mydir.isDirectory()))) {mydir.mkdir();}
-			mydir = new File(myString+"/sub1/sub2");
-			if ((!(mydir.exists()) || (!mydir.isDirectory()))) {mydir.mkdir();}
+
+		 System.out.println("Chemin du fichier texte à copier :");
+		 String otherString = new String(sc.nextLine());
+		 mydir = new File(otherString);
+		 try {
+			  String pathname = otherString.substring(otherString.lastIndexOf("/"));
+			FileOutputStream myDirFile = new FileOutputStream(new File(myString + "/"+pathname));
+			try {
+				myDirFile.write(myString.getBytes());
+				myDirFile.write("\n\r".getBytes());
+				
+				myDirFile.write(date);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		//Directory
-		mydir = new File(myString);
+	}
+
+	private static void writeDirFile(String dirpathname) {
+		File mydir = new File(dirpathname);
 		try {
-			FileOutputStream myDirFile = new FileOutputStream(new File(myString+"/.directory"));
-			
-			for(String stFileName : mydir.list()){
+			FileOutputStream myDirFile = new FileOutputStream(new File(dirpathname + "/.directory"));
+
+			for (String stFileName : mydir.list()) {
 				try {
 					myDirFile.write(stFileName.getBytes());
 					myDirFile.write("\n\r".getBytes());
@@ -54,14 +82,7 @@ public class MainIo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-//		System.out.println("Chemin du fichier texte à copier :");
-//		myString = new String(sc.nextLine());
-//		File mydir = new File(myString);
-		
+
 	}
-	
+
 }
